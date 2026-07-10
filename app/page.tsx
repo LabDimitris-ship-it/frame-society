30f9db61528a2bf8b2e93053dca21a06d89167fc
Dimitris Liofis <SophilenaDimitris@outlook.com>
2026-07-10 15:25:08 +0300

    Use HomeV3 for all devices

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,5 @@
 import HomeV3 from "./home-v3/page";
-import HomeMobile from "@/components/mobile/v2/HomeMobile";
 
 export default function Home() {
-  return (
-    <>
-      <div className="hidden md:block">
-        <HomeV3 />
-      </div>
-
-      <div className="md:hidden">
-        <HomeMobile />
-      </div>
-    </>
-  );
+  return <HomeV3 />;
 }
